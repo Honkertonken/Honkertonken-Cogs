54cc69d8f817c19237fa2860d24feda6a696668d
Honkertonken <prakhar.taparia.10@gmail.com>
2022-01-10 10:35:54 +0530

    add support for reply

--- Whoasked/whoasked.py
+++ Whoasked/whoasked.py
@@ -4,14 +4,22 @@ import discord
 from redbot.core import commands
 
 
+def reply(ctx):
+    if hasattr(ctx.message, "reference") and ctx.message.reference is not None:
+        msg = ctx.message.reference.resolved
+        if isinstance(msg, discord.Message):
+            return msg
+
+
 class WhoAsked(commands.Cog):
     """When you just have to ask who the hell asked?"""
 
     @commands.command()
-    async def whoasked(self, ctx, message: discord.PartialMessage):
+    async def whoasked(self, ctx, *, reply_or_message: str = None):
         """Who Asked?"""
-        try:
-            m = await message.reply(
+        if resp := reply(ctx):
+            message_id = await ctx.fetch_message(resp.id)
+            m = await message_id.reply(
                 "Now playing:\nWho Asked (Feat. Nobody Did)\n⚪──────────────\n◄◄⠀▐▐⠀►► 0:00 / 4:42⠀───○ 🔊"
             )
             await asyncio.sleep(1)
@@ -35,5 +43,32 @@ class WhoAsked(commands.Cog):
                 content="Now playing:\nWho Asked (Feat. Nobody Did)\n──────────────⚪\n◄◄⠀▐▐⠀►► 4:42 / 4:42⠀───○ 🔊"
             )
 
-        except discord.HTTPException:
-            await ctx.send("Incorrect/Invalid Message Id.")
+        else:
+            try:
+                message_id = await ctx.fetch_message(reply_or_message)
+                m = await message_id.reply(
+                    "Now playing:\nWho Asked (Feat. Nobody Did)\n⚪──────────────\n◄◄⠀▐▐⠀►► 0:00 / 4:42⠀───○ 🔊"
+                )
+                await asyncio.sleep(1)
+                await m.edit(
+                    content="Now playing:\nWho Asked (Feat. Nobody Did)\n───⚪───────────\n◄◄⠀▐▐⠀►► 1:34 / 4:42⠀───○ 🔊"
+                )
+                await asyncio.sleep(1)
+                await m.edit(
+                    content="Now playing:\nWho Asked (Feat. Nobody Did)\n──────⚪────────\n◄◄⠀▐▐⠀►► 2:21 / 4:42⠀───○ 🔊"
+                )
+                await asyncio.sleep(1)
+                await m.edit(
+                    content="Now playing:\nWho Asked (Feat. Nobody Did)\n─────────⚪─────\n◄◄⠀▐▐⠀►► 3:08 / 4:42⠀───○ 🔊"
+                )
+                await asyncio.sleep(1)
+                await m.edit(
+                    content="Now playing:\nWho Asked (Feat. Nobody Did)\n────────────⚪──\n◄◄⠀▐▐⠀►► 3:55 / 4:42⠀───○ 🔊"
+                )
+                await asyncio.sleep(1)
+                await m.edit(
+                    content="Now playing:\nWho Asked (Feat. Nobody Did)\n──────────────⚪\n◄◄⠀▐▐⠀►► 4:42 / 4:42⠀───○ 🔊"
+                )
+
+            except discord.HTTPException:
+                await ctx.send("No reply found/invalid message id.")
