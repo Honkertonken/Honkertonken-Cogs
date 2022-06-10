9b8fd8cc7f11d30ffbec3e1b4376f5d0da8fdb8c
Honkertonken <prakhar.taparia.10@gmail.com>
2022-06-10 21:17:13 +0530

    add random toggle to pp

--- pp/pp.py
+++ pp/pp.py
@@ -2,14 +2,18 @@ import random
 
 import discord
 from redbot.core import commands
+from redbot.core.config import Config
 from redbot.core.utils.chat_formatting import pagify
 
 
 class Pp(commands.Cog):
-    """pp"""
+    """Shows your or someone else's pp Note - 100% accurate"""
 
     def __init__(self, bot):
         self.bot = bot
+        default = {"random": True}
+        self.config = Config.get_conf(self, 69696969, force_registration=True)
+        self.config.register_global(**default)
 
     async def red_delete_data_for_user(self, **kwargs):
         """
@@ -18,30 +22,40 @@ class Pp(commands.Cog):
         return
 
     @commands.command()
+    @commands.is_owner()
+    async def randompp(self, ctx, random: bool):
+        """
+        Sets whether or not to generate a new pp length ever time the command
+        is ran.
+        """
+        await self.config.random.set(random)
+        await ctx.send("Random pp length is now {}.".format("enabled" if random else "disabled"))
+
+    @commands.command()
     async def pp(self, ctx, *users: discord.Member):
         """
-        Detects user's pp length This is 100% accurate.
+        Detects user's pp length.
 
-        Enter multiple users for an accurate comparison!
+        Note : This is 100% accurate.
+
+        Enter multiple users for a comparison!
 
         """
         if not users:
             users = {ctx.author}
 
         lengths = {}
-        state = random.getstate()
 
         for user in users:
-            random.seed(str(user.id))
+            random.seed(None) if await self.config.random() else random.seed(str(user.id))
 
             if user.id == ctx.bot.user.id or user.id in list(ctx.bot.owner_ids):
-                length = 35
+                length = random.randint(30, 35)
             else:
                 length = random.randint(0, 30)
 
             lengths[user] = f'8{"=" * length}D'
 
-        random.setstate(state)
         lengths = sorted(lengths.items(), key=lambda x: x[1])
 
         msg = "".join(
