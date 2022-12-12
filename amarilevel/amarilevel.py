32f56cc144d3e17aa13da01d3d2526a4b18288cb
Honkertonken <prakhar.taparia.10@gmail.com>
2022-12-12 19:30:18 +0530

    [amari] token check

--- amarilevel/amarilevel.py
+++ amarilevel/amarilevel.py
@@ -18,32 +18,35 @@ class AmariLevel(commands.Cog):
         """
         View your amari rank.
         """
-        if not member:
-            member = ctx.author
-        if member.bot:
-            return await ctx.send("Bots dont have any amari xp smh.")
-        if ctx.guild.get_member(339254240012664832):
-            token = (await self.bot.get_shared_api_tokens("amari")).get("auth")
-            bot_info = await self.bot.application_info()
-            amari = AmariClient(token)
-            try:
-                lb = await amari.fetch_full_leaderboard(ctx.guild.id)
-                user = lb.get_user(member.id)
-                e = discord.Embed(
-                    title=f"{member.name}'s Amari Rank",
-                    type="rich",
-                    color=await ctx.embed_color(),
-                    description=f"**Rank : {user.position+1}\nLevel : {user.level}\nXp : {user.exp}\n Weekly Xp : {user.weeklyexp}**",
-                )
-                e.set_author(name=f"{member.display_name}", icon_url=f"{member.avatar_url}")
-                e.set_footer(text=f"{ctx.guild.name}", icon_url=f"{ctx.guild.icon_url}")
-                await ctx.send(embed=e)
-            except NotFound:
-                await ctx.send(f"{member} has no amari data in {ctx.guild}.")
-            except InvalidToken:
-                await ctx.send(
-                    f"The Amari token is invalid, please report this to {bot_info.owner}."
-                )
-            await amari.close()
+        token = (await self.bot.get_shared_api_tokens("amari")).get("auth")
+        if token:
+            if not member:
+                member = ctx.author
+            if member.bot:
+                return await ctx.send("Bots dont have any amari xp smh.")
+            if ctx.guild.get_member(339254240012664832):
+                bot_info = await self.bot.application_info()
+                amari = AmariClient(token)
+                try:
+                    lb = await amari.fetch_full_leaderboard(ctx.guild.id)
+                    user = lb.get_user(member.id)
+                    e = discord.Embed(
+                        title=f"{member.name}'s Amari Rank",
+                        type="rich",
+                        color=await ctx.embed_color(),
+                        description=f"**Rank : {user.position+1}\nLevel : {user.level}\nXp : {user.exp}\n Weekly Xp : {user.weeklyexp}**",
+                    )
+                    e.set_author(name=f"{member.display_name}", icon_url=f"{member.avatar_url}")
+                    e.set_footer(text=f"{ctx.guild.name}", icon_url=f"{ctx.guild.icon_url}")
+                    await ctx.send(embed=e)
+                except NotFound:
+                    await ctx.send(f"{member} has no amari data in {ctx.guild}.")
+                except InvalidToken:
+                    await ctx.send(
+                        f"The Amari token is invalid, please report this to {bot_info.owner}."
+                    )
+                await amari.close()
+            else:
+                await ctx.send(f"You dont have Amaribot in {ctx.guild.name}.")
         else:
-            await ctx.send(f"You dont have Amaribot in {ctx.guild.name}.")
+            await ctx.send("Please set an amari api token first.")
